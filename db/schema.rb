35dceaac5e9d4597df41dfe675300753b81bb8f5
Khan <Talhaakhan@pm.me>
2022-04-15 18:48:52 -0700

    heroku -live and free and free

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_143635) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_16_014712) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -85,7 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_143635) do
     t.string "ssn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "dob"
+    t.string "dob"
     t.index ["merchant_id"], name: "index_identity_verifications_on_merchant_id"
   end
 
