cd5b969991e334c4f4762bd9ed188c3efbb519fb
Khan <Talhaakhan@pm.me>
2022-04-15 23:47:19 -0700

    some chantes

--- db/migrate/20220416014712_add_column_to_identity_verification_hello.rb
+++ db/migrate/20220416014712_add_column_to_identity_verification_hello.rb
@@ -1,6 +1,5 @@
 class AddColumnToIdentityVerificationHello < ActiveRecord::Migration[7.0]
   def change
-    change_column :identity_verifications, :dob, :string
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
--- db/migrate/20220416022048_remove_column_from_table.rb
+++ db/migrate/20220416022048_remove_column_from_table.rb
@@ -1,5 +1,4 @@
 class RemoveColumnFromTable < ActiveRecord::Migration[7.0]
   def change
-    remove_column :identity_verifications, :dob
   end
 end
