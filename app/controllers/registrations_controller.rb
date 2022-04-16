6032308ea4e03da8343d838ce68a4e5a6d44546b
Khan <Talhaakhan@pm.me>
2022-04-15 18:32:10 -0700

    scaffold readyy

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    static_pages_about_path # Or :prefix_to_your_route
+    static_pages_index_path # Or :prefix_to_your_route
   end
 end
