f1929e52dbe6532d8d809e20b8df8b4a0ad6202e
Khan <Talhaakhan@pm.me>
2022-04-15 23:43:28 -0700

    some chantes

--- db/migrate/20220415085004_add_column_to_identity_verification.rb
+++ db/migrate/20220415085004_add_column_to_identity_verification.rb
@@ -1,6 +1,5 @@
 class AddColumnToIdentityVerification < ActiveRecord::Migration[7.0]
   def change
-    add_column :identity_verifications, :dob, :duedate
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
